08f4e59f4e49844076a1bef35619616fb368036a
twanas <antoine@reltech.com>
2019-10-22 12:42:04 +1100

    Removed shared lib support

--- conanfile.py
+++ conanfile.py
@@ -8,8 +8,6 @@ class PapiConan(ConanFile):
     url = "https://github.com/antoinewaugh/conan-papi"
     description = "The Performance Application Programming Interface (PAPI) provides tool designers and application engineers with a consistent interface and methodology for the use of low-level performance counter hardware"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
 
     def source(self):
         self.run("git clone https://bitbucket.org/icl/papi.git")
@@ -17,18 +15,12 @@ class PapiConan(ConanFile):
     def build(self):
         os.chdir("papi/src")
         autotools = AutoToolsBuildEnvironment(self)
-        if self.options.shared:
-            autotools.configure(args=["--with-shared-lib=yes"])
-        else:
-            autotools.configure(args=["--with-shared-lib=no"])
+        autotools.configure(args=["--with-shared-lib=no"])
         autotools.make()
 
     def package(self):
         self.copy("*.h", dst="include", src="papi/src")
-        if self.options.shared:
-            self.copy("*.so", dst="lib", src="papi/src", keep_path=False)
-        else:
-            self.copy("*.a", dst="lib", src="papi/src", keep_path=False)
+        self.copy("*.a", dst="lib", src="papi/src", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["papi"]
